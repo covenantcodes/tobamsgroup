801ff10537809ae0629db703932d387b53eb4e0a
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2023-08-31 12:35:13 +0100

    Created Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div>
-        <div className='nav_bar'>
-            <div className="logo_container">
-                <img src="../public/img/Logo.png" alt="" />
-            </div>
+      <div className="nav_bar">
+        <div className="logo_container">
+          <img src="../public/img/Logo.png" alt="" />
+        </div>
 
-            <ul className='nav_links_container'>
-                <li className='nav_links'>Store</li>
-                <li className='nav_links'>About</li>
-                <li className='nav_links'>Contact</li>
-                <li className='nav_links'>Blog</li>
-            </ul>
+        <ul className="nav_links_container">
+          <li className="nav_links">Store</li>
+          <li className="nav_links">About</li>
+          <li className="nav_links">Contact</li>
+          <li className="nav_links">Blog</li>
+        </ul>
 
-            <div className="search_bar">
-              
-            </div>
+        <div className="search_bar">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text" placeholder="Search" className="search_input" />
+        </div>
+
+        <div className="left_nav_items">
 
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App  
+export default App;
