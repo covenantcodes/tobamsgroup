a24ddaa502a7ce584790a14f5e1163e6b25cdddd
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2023-08-31 12:14:13 +0100

    Created nav bar and Nav Links

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,24 @@ import './App.css'
 function App() {
 
   return (
-    <>
-        <h1>jkdahkjs</h1>
-    </>
+    <div>
+        <div className='nav_bar'>
+            <div className="logo_container">
+                <img src="../public/img/Logo.png" alt="" />
+            </div>
+
+            <ul className='nav_links_container'>
+                <li className='nav_links'>Store</li>
+                <li className='nav_links'>About</li>
+                <li className='nav_links'>Contact</li>
+                <li className='nav_links'>Blog</li>
+            </ul>
+
+            <div className="search_bar">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
